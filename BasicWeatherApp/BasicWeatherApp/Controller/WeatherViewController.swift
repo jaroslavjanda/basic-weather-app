d5b9b17175d341486be22b15ba4e432072379748
Jaroslav <jaroslav.janda@plus4u.net>
2019-06-19 17:07:42 +0200

    add location

--- BasicWeatherApp/BasicWeatherApp/Controller/WeatherViewController.swift
+++ BasicWeatherApp/BasicWeatherApp/Controller/WeatherViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class WeatherViewController: UIViewController {
+    
+    // MARK: - UI
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBOutlet weak var weatherIcon: UIImageView!
+    
+    // MARK: - Constants
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "639321efa312f94d442150666ebd773e"
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        }
     }
+}
 
-
+ extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        
+    }
 }
 
