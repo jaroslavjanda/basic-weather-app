5abfeaf886d7a49cbf94b86221d5a02a51898b26
Jaroslav <jaroslav.janda@plus4u.net>
2019-06-19 17:26:42 +0200

    fix .git

--- BasicWeatherApp/BasicWeatherApp/WeatherDataModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  WeatherDataModel.swift
-//  BasicWeatherApp
-//
-//  Created by Jaroslav Janda on 19/06/2019.
-//  Copyright © 2019 Jaroslav Janda. All rights reserved.
-//
-
-import Foundation
