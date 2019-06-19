f555a3c7d6bd0c7e3b75f8cbf6ee594a677363f3
Jaroslav <jaroslav.janda@plus4u.net>
2019-06-19 13:20:28 +0200

    MVC architecture

---
+++ BasicWeatherApp/BasicWeatherApp/WeatherDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherDataModel.swift
+//  BasicWeatherApp
+//
+//  Created by Jaroslav Janda on 19/06/2019.
+//  Copyright © 2019 Jaroslav Janda. All rights reserved.
+//
+
+import Foundation
