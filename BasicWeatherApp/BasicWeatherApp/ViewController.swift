281fc79b4d799aa31b78656b283d215a9c5cca59
Jaroslav <jaroslav.janda@plus4u.net>
2019-06-19 13:14:09 +0200

    Create project

---
+++ BasicWeatherApp/BasicWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicWeatherApp
+//
+//  Created by Jaroslav Janda on 19/06/2019.
+//  Copyright © 2019 Jaroslav Janda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
