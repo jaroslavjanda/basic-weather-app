59f9f567949c91166baa9a9718ed918ade5a2e52
Jaroslav <janda.jaroslav5@gmail.com>
2019-07-09 12:02:54 +0200

    create request params

--- BasicWeatherApp/Controller/WeatherViewController.swift
+++ BasicWeatherApp/Controller/WeatherViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreLocation
+import Alamofire
 
 class WeatherViewController: UIViewController {
     
@@ -19,8 +20,10 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var weatherIcon: UIImageView!
     
     // MARK: - Constants
-    //let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    //let APP_ID = "639321efa312f94d442150666ebd773e"
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "639321efa312f94d442150666ebd773e"
+    let UNITS = "metric"
+    
     var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
@@ -40,8 +43,19 @@ class WeatherViewController: UIViewController {
  extension WeatherViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
-        print("locations = \(locValue.latitude) \(locValue.longitude)")
-        locationManager.stopUpdatingLocation()
+        
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+        }
+        
+        let params: [String: String] = ["id": APP_ID, "units": UNITS, "lat": String(locValue.latitude), "lac": String(locValue.longitude)]
+        getWeatherData(URL: WEATHER_URL, params: params)
     }
 }
 
+private extension WeatherViewController {
+    func getWeatherData(URL: String, params: [String: String]) {
+        
+    }
+}
